fcc78296d0744b4f3afd66521c278674c3d35c26
Juan Motta <juan.motta@simetrik.com>
2024-11-05 11:15:56 -0500

    feat: add flag to avoid notifications at night

--- main.py
+++ main.py
@@ -5,21 +5,30 @@ import os
 WEB_SERVER_URL = os.getenv("WEB_SERVER_URL")
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 TIME = os.getenv("TIME", 60)
-
+NOTIFY_AT_NIGHT = os.getenv("NOTIFY_AT_NIGHT", "false")
 
 def query_web_server():
     try:
+        print(WEB_SERVER_URL)
         response = requests.get(WEB_SERVER_URL)
+        print(f"Status code: {response.status_code} at {time.strftime("%H:%M")}")
         response.raise_for_status()
     except requests.RequestException as e:
         send_failure_to_discord(str(e))
 
 
 def send_failure_to_discord(message):
-    requests.post(
-        DISCORD_WEBHOOK_URL,
-        json={"username": "test", "content": "hello"},
-    )
+    notify = True
+    # Notify only during the day
+    if NOTIFY_AT_NIGHT == "true":
+        hour = int(time.strftime("%H"))
+        if hour < 8 or hour > 20:
+            notify = False
+    if notify:
+        requests.post(
+            DISCORD_WEBHOOK_URL,
+            json={"username": "Error backend", "content": message},
+        )
 
 
 if __name__ == "__main__":
